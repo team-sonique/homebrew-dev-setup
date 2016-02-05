7457267187f0114ed77c60e085ede6334572d58a
Sonique <team.sonique.sns@bskyb.com>
2016-02-05 10:37:31 +0000

    TimW: added cask for java 1.8.0_66

---
+++ Casks/java8u66.rb
@@ -0,0 +1,33 @@
+cask 'java8u66' do
+  version '1.8.0_66'
+  sha256 'cd416de4f41f9f0a6984c456481437681674a717da62259740c54732f174fa08'
+
+  url 'http://download.oracle.com/otn-pub/java/jdk/8u66-b17/jdk-8u66-macosx-x64.dmg',
+      :cookies => {
+                    'oraclelicense' => 'accept-securebackup-cookie'
+                  }
+  homepage 'http://www.oracle.com/technetwork/java/javase/downloads/index.html'
+  license :unknown
+
+  pkg 'JDK 8 Update 66.pkg'
+  postflight do
+    system '/usr/bin/sudo', '-E', '--',
+      '/usr/libexec/PlistBuddy', '-c', 'Add :JavaVM:JVMCapabilities: string BundledApp', "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents/Info.plist"
+    system '/usr/bin/sudo', '-E', '--',
+      '/usr/libexec/PlistBuddy', '-c', 'Add :JavaVM:JVMCapabilities: string JNI',        "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents/Info.plist"
+    system '/usr/bin/sudo', '-E', '--',
+      '/usr/libexec/PlistBuddy', '-c', 'Add :JavaVM:JVMCapabilities: string WebStart',   "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents/Info.plist"
+    system '/usr/bin/sudo', '-E', '--',
+      '/usr/libexec/PlistBuddy', '-c', 'Add :JavaVM:JVMCapabilities: string Applets',    "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents/Info.plist"
+    system '/usr/bin/sudo', '-E', '--',
+      '/bin/rm', '-rf', '--', '/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK'
+    system '/usr/bin/sudo', '-E', '--',
+      '/bin/ln', '-nsf', '--', "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents", '/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK'
+    system '/usr/bin/sudo', '-E', '--',
+      '/bin/mkdir', '-p', '--', "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents/Home/bundle/Libraries"
+    system '/usr/bin/sudo', '-E', '--',
+      '/bin/ln', '-nsf', '--', "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents/Home/jre/lib/server/libjvm.dylib", "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents/Home/bundle/Libraries/libserver.dylib"
+  end
+  uninstall :pkgutil => 'com.oracle.jdk8u66',
+            :delete => "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk"
+end
