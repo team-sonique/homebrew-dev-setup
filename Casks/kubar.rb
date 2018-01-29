1579b0c91ecf7c2ebea667efea205a5ce109b425
Team Sonique <team.sonique.sns@gmail.com>
2018-01-29 10:06:13 +0000

    Jon: Don't check sha

--- Casks/kubar.rb
+++ Casks/kubar.rb
@@ -1,6 +1,6 @@
 cask 'kubar' do
   version '2.1'
-  sha256 'af9f484e05ed9ac6e174a72371c9ae9821f24d661189eb88326abfe23d4b9809'
+  sha256 :no_check
 
   url "https://git.sns.sky.com/jonathan.sharifi/kubar/raw/#{version}/binary/Kubar.dmg"
   homepage 'https://git.sns.sky.com/jonathan.sharifi/kubar/'
