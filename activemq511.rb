feec1e3ac1fb74dce644374929dfcd1e95680417
Team Sonique <team.sonique.sns@gmail.com>
2020-07-08 16:22:32 +0100

    Create activemq511.rb

---
+++ activemq511.rb
@@ -0,0 +1,43 @@
+class Activemq511 < Formula
+  desc "Apache ActiveMQ version 5.11.4"
+  homepage "https://activemq.apache.org/"
+  url "http://archive.apache.org/dist/activemq/5.11.4/apache-activemq-5.11.4-bin.tar.gz"
+  sha256 "b2076bca6964371a41c420b0db047d027ab81617"
+
+  depends_on :java => "1.6+"
+
+  def install
+    rm_rf Dir["bin/linux-x86-*"]
+    libexec.install Dir["*"]
+    (bin/"activemq").write_env_script libexec/"bin/activemq", Language::Java.java_home_env("1.6+")
+    (bin/"activemq-admin").write_env_script libexec/"bin/activemq-admin", Language::Java.java_home_env("1.6+")
+  end
+
+  plist_options :manual => "activemq start"
+
+  def plist; <<-EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
+    "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{libexec}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/activemq</string>
+          <string>start</string>
+        </array>
+      </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    system "#{bin}/activemq-admin", "browse", "-h"
+  end
+end
