7e54f96be03223967835b05a872f9d4037c85ee0
Sonique <team.sonique.sns@bskyb.com>
2021-05-18 16:02:27 +0100

    Fix style

--- kubernetes-helm.rb
+++ kubernetes-helm.rb
@@ -1,17 +1,16 @@
 class KubernetesHelm < Formula
-  desc "The Kubernetes package manager"
+  desc "Kubernetes package manager"
   homepage "https://helm.sh/"
   url "https://github.com/helm/helm.git",
-      :tag      => "v2.16.1",
-      :revision => "bbdfe5e7803a12bbdf97e94cd847859890cf4050"
+      tag:      "v2.16.1",
+      revision: "bbdfe5e7803a12bbdf97e94cd847859890cf4050"
   head "https://github.com/helm/helm.git"
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles"
-    cellar :any_skip_relocation
-    sha256 "e13b6d2b1a76b4af452e8582f2d2b07cde2b231de00ce16be04dbbf0c77b49af" => :catalina
-    sha256 "e41f332120fc3a047b2b654a6afb286c9a09e24a0ac5929ba943cc94940d1267" => :mojave
-    sha256 "29683ee842eaebd7a88c8c1f9e9069c12f6c8c0b531ff9255fe97330da58d2d8" => :high_sierra
+    sha256 cellar: :any_skip_relocation, catalina:    "e13b6d2b1a76b4af452e8582f2d2b07cde2b231de00ce16be04dbbf0c77b49af"
+    sha256 cellar: :any_skip_relocation, mojave:      "e41f332120fc3a047b2b654a6afb286c9a09e24a0ac5929ba943cc94940d1267"
+    sha256 cellar: :any_skip_relocation, high_sierra: "29683ee842eaebd7a88c8c1f9e9069c12f6c8c0b531ff9255fe97330da58d2d8"
   end
 
   depends_on "glide" => :build
@@ -33,10 +32,10 @@ class KubernetesHelm < Formula
       bin.install "bin/tiller"
       man1.install Dir["docs/man/man1/*"]
 
-      output = Utils.popen_read("SHELL=bash #{bin}/helm completion bash")
+      output = Utils.safe_popen_read({ "SHELL" => "bash" }, "#{bin}/helm", "completion", "bash")
       (bash_completion/"helm").write output
 
-      output = Utils.popen_read("SHELL=zsh #{bin}/helm completion zsh")
+      output = Utils.safe_popen_read({ "SHELL" => "zsh" }, "#{bin}/helm", "completion", "zsh")
       (zsh_completion/"_helm").write output
 
       prefix.install_metafiles
@@ -49,6 +48,9 @@ class KubernetesHelm < Formula
 
     version_output = shell_output("#{bin}/helm version --client 2>&1")
     assert_match "GitTreeState:\"clean\"", version_output
-    assert_match stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:revision], version_output if build.stable?
+    if build.stable?
+      assert_match stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:revision],
+version_output
+    end
   end
 end
