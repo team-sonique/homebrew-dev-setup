317b4bd2fbca62f851e8a65ca9d6ef579f4abf57
Tim Woolford <976550+TimWoolford@users.noreply.github.com>
2021-09-08 09:00:04 +0100

    update kubectl-login-plugin url

--- kubectl-login-plugin.rb
+++ kubectl-login-plugin.rb
@@ -1,10 +1,10 @@
 class KubectlLoginPlugin < Formula
   desc "Kubectl Login Plugin"
   homepage ""
-  version "1.5.29"
-  url "http://repo.sns.sky.com:8081/artifactory/libs-releases/sky/dost/k8s/kubectl-login/kubectl-login-darwin-amd64-#{version}.tgz",
+  version "1.5.43"
+  url "http://artifactory.bmtapps.bskyb.com/artifactory/libs-releases/sky/dost/k8s/kubectl-login/kubectl-login-darwin-amd64-#{version}.tgz",
       :using => :nounzip
-  sha256 "867e16e957e5ae04cf5df0d8db2caa2709d867ba878fdec425bd8dff48da446a"
+  sha256 "677bce351dbacb6b9d1d0a001d44fad7ad515c9fd272b0370604240cdb97cbc1"
   license ""
 
   def install
